f261a3b589c730bd4bf09274eb27b2e7a7c3f7ed
octaviopedroza-univer <octavio.pedroza@univer-gdl.edu.mx>
2022-10-17 12:30:16 -0500

    Se agregan elementos en archivo de dom

--- 5.DOM_BOM/modifica_elementos.js
+++ 5.DOM_BOM/modifica_elementos.js
@@ -3,11 +3,24 @@
 //Este es el documento que utiliza el DOM para hacer modificanes
 
 // conseguir elementos por su ID concreto**********************************************************************
+// EL document.getElementById() nos sirve para seleccionar elementos html por su id el cual se ingresa dentro de los parentesis
+var box = document.getElementById("micaja");
+console.log(box);
+
+//con el innerHTML devolvemos lo que tiene guardado el elemento html con el id ingresado
+var box2 = document.getElementById("micaja").innerHTML;
+console.log(box2);
+
+//Cambiar texto de un elemento seleccionado por id con innerHTML desde JS
+box.innerHTML = "Nuevo texto en Mi caja";//como ya existe la variable solo accedemos a su metodo .innerHTML.
+
+
 function cambiaColor(color){
     caja.style.background = color;
 }
 
-// var caja = document.getElementById("caja1");
+/* Practicamente es lo mismo que getElementById solo que es mas parecido a CSS, para seleccionar un id debemos usar "#" y 
+para seleccionar alguna clase debemos usar ".", es solo una manera mas elegante de hacer las cosas*/
 var caja = document.querySelector("#caja1");
 caja.innerHTML = "¡Texto en la caja desde JS";
 caja.style.background = "red";
@@ -21,7 +34,6 @@ console.log(todosLosDivs);
 
 var cont = todosLosDivs[2].textContent;
 console.log(cont);
-
 
 for(var valor in todosLosDivs){
     var parrafo = document.createElement("p");
@@ -38,3 +50,9 @@ console.log(divsRojos);
 var divsAmarillos = document.getElementsByClassName('amarillo');
 divsAmarillos[0].style.background = "yellow";
 console.log(divsAmarillos);
+
+//conseguir elemento por su nombre *********************************************************************************
+var nombreElemento = document.getElementsByName("cuarto");
+nombreElemento[0].style.color = "green";
+
+

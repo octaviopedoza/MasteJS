70769c899942b9085ae201df66effedf8b4c8dda
octaviopedroza-univer <octavio.pedroza@univer-gdl.edu.mx>
2022-10-21 15:56:44 -0500

    se añaden documentos de selectores para jQuery

--- 2.JQuery/2.-Selectores/selectores.js
+++ 2.JQuery/2.-Selectores/selectores.js
@@ -3,9 +3,30 @@ es decir, nosotros podemos seleccionar cualquier cosa y despues utilizar cualqui
 donde se ejecuta lo que necesitamos.
 
 Al utilizar el selector document nos estamos refiriendo a toda la pagina web en general y espera a que se carguen todo los script
-y luego utilizamos el metodo "ready" y despues tiene una funcion de callback y aqui dentro ejecutamos la funcionalidad que nosotros queramos
-en este caso es un "console.log" */
+y luego utilizamos el metodo "ready" y despues tiene una funcion de callback y aqui dentro ejecutamos la funcionalidad que nosotros 
+queramos en este caso es un "console.log" */
 
 $(document).ready(function(){
     console.log("Estamos listos");
+
+// Selectores de ID *******************************************************************************************************
+$("#rojo").css("background","red")
+                    .css("color","white");
+$("#amarillo").css("background", "yellow")
+                    .css("color", "green");
+
+$("#verde").css("background", "green")
+                    .css("color", "white");
+
+//Selectores de clase ******************************************************************************************************
+var mi_clase = $('.zebra');
+mi_clase.css("border", "5px dashed black");
+
+//Selectores por etiqueta **************************************************************************************************
+var parrafos = $('p');
+parrafos.click(function(){
+    $(this).removeClass("zebra");
+    console.log("click tag");
+});
+
 });

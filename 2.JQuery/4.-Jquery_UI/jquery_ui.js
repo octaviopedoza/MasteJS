0b7c3b4fd13031017780dca73f35dca0ceb49e50
octaviopedroza-univer <octavio.pedroza@univer-gdl.edu.mx>
2022-10-26 13:50:02 -0500

    Se agrega primer parte del proyecyo de JS y jQuery

--- 2.JQuery/4.-Jquery_UI/jquery_ui.js
+++ 2.JQuery/4.-Jquery_UI/jquery_ui.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $('.elemento').resizable();
 
     /*Lista seleccionable y ordenable
-    Nota: solo podemos utilizar un metodo de estos a la vez */
+    Nota: solo podemos utilizar un metodo de estos a la vez *************************************************************************/
     //$('.lista-seleccionable').selectable();
     $('.lista-seleccionable').sortable({
         update: function(event, ui){
@@ -14,7 +14,40 @@ $(document).ready(function(){
         }
     });
 
-    //Drop
+    //Drop****************************************************************************************************************************
     $("#elemento-movido").draggable();
-    $("#area").droppable();
+    $("#area").droppable({
+        drop: function(){
+            console.log("has sltado algo dentro del area");
+        }
+    });
+
+    /* Efectos dentro de jQuery_UI **************************************************************************************************/
+    $("#mostrar").click(function(){
+        $(".caja-efectos").toggle("fade");//muestra el div al precionar el boton con un efecto de fade
+        // $(".caja-efectos").toggle("explode");
+        // $(".caja-efectos").toggle("blind");
+        // $(".caja-efectos").toggle("slide");
+        // $(".caja-efectos").toggle("drop");
+        // $(".caja-efectos").toggle("fold");
+        // $(".caja-efectos").toggle("puff");
+        // $(".caja-efectos").toggle("scale");
+        // $(".caja-efectos").toggle("shake");
+    /*de igual forma podemos pasar como 2° parametro el tiempo o velocidad que queremos para el efecto 
+    ("shake",4000) o ("shake",slow)**************************************************************************************************/
+    
+    /* Plugins y Widgets dentro de jQuery_UI */
+    $(document).tooltip();
+    });
+
+    /* Dialog o cuadros de texto */
+    $("#lanzar-popup").click(function(){
+        $("#popup").dialog();
+    });
+
+    /* Datepicker o calendario */
+    $("#calendario").datepicker();
+    
+    /* Taps o pestañas */
+    $("#pestanas").tabs();
 });

f8039fdbeca64ae505bc238f108034052f126038
octaviopedroza-univer <octavio.pedroza@univer-gdl.edu.mx>
2022-11-01 18:58:39 -0600

    Se comienza con TypeScript

---
+++ 3.-Typescript/2.-variables/variables.js
@@ -0,0 +1,13 @@
+//Strings
+var cadena = "Octavio pedroza";
+//Number
+var numero = 12;
+//Boleano
+var verdaro_falso = true;
+//Any
+var cualquiera = "hola";
+cualquiera = 77;
+//Array
+var lenguajes = ["PHP", "JS", "HTML"];
+var years = [12, 13, 15];
+console.log(cadena, numero, verdaro_falso, cualquiera, lenguajes, years);
---
+++ 3.-Typescript/2.-variables/variables.ts
@@ -0,0 +1,18 @@
+//Strings
+let cadena: string = "Octavio pedroza";
+
+//Number
+let numero: number = 12;
+
+//Boleano
+let verdaro_falso: boolean = true;
+
+//Any
+let cualquiera: any ="hola";
+cualquiera = 77;
+
+//Array
+var lenguajes: Array <string> = ["PHP", "JS", "HTML"];
+let years: number[] = [12, 13, 15];
+
+console.log(cadena, numero, verdaro_falso, cualquiera, lenguajes, years);

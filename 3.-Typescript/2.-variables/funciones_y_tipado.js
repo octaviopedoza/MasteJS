8e5c8a84a2d23177defdf76c4ea0909c17c44f07
octaviopedroza-univer <octavio.pedroza@univer-gdl.edu.mx>
2022-11-07 17:47:47 -0600

    Se finasliza el bloque de Typescript

--- 3.-Typescript/2.-variables/funciones_y_tipado.js
+++ 3.-Typescript/2.-variables/funciones_y_tipado.js
@@ -1,14 +1,47 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //Funciones
 function getNumero(numero) {
     if (numero === void 0) { numero = 12; }
     return "el numero es " + numero;
 }
 console.log(getNumero(10));
+//Decoradores
+/* Es un patrón de diseño que nos permite mediante unos metadatos que nosotros le definimos de acuerdo a una clase hacer una copia de
+esa misma clase y modificarla o que haga una cosa u otras en función de los parámetros que le pase de acuerdo.*/
+// function estampar(logo: string){
+//     return function(target: Function){
+//         target.prototype.estampacion = function():void{
+//             console.log("Camiseta estampada "+logo);
+//         }
+//     }
+// }
 //Clases (modelo de objeto)
+// @estampar('CK')
 var Camiseta = /** @class */ (function () {
-    function Camiseta() {
+    /*Metodos (funciones o acciones del objeto)
+    el Constructor es un metodo especial dentro de una clase y se utiliza para darle un valor inicial a los atributos
+    o propiedades del objeto al crearlo, el constructor nunca devuelve ningun dato*/
+    function Camiseta(color, modelo, marca, talla, precio) {
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.talla = talla;
+        this.precio = precio;
     }
-    //Metodos (funciones o acciones del objeto)
     Camiseta.prototype.setColor = function (color) {
         this.color = color;
     };
@@ -17,11 +50,25 @@ var Camiseta = /** @class */ (function () {
     };
     return Camiseta;
 }());
-var camiseta = new Camiseta();
+// Clase hija(Ejemplo de herencia)
+// Es necesario agregar el extends para pder agregar las propiedades de la clase padre.
+var Sudadera = /** @class */ (function (_super) {
+    __extends(Sudadera, _super);
+    function Sudadera() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Sudadera.prototype.setCapucha = function (capucha) {
+        this.capucha = capucha;
+    };
+    Sudadera.prototype.getCapucha = function () {
+        return this.capucha;
+    };
+    return Sudadera;
+}(Camiseta));
+//Pasamos por parametro los atributos definidos dentro del constructor
+var camiseta = new Camiseta("Rojo", "Manga costa", "Polo", "Grande", 14);
 camiseta.setColor("Rojo");
-camiseta.precio = 250;
-var playera = new Camiseta();
-playera.setColor("Azul");
-playera.precio = 500;
 console.log(camiseta);
-console.log(playera);
+var sudadera_nike = new Sudadera("Blanca", "Manga larga", "Sudadera", "Grande", 25);
+sudadera_nike.setCapucha(true);
+console.log(sudadera_nike);
--- 3.-Typescript/2.-variables/funciones_y_tipado.ts
+++ 3.-Typescript/2.-variables/funciones_y_tipado.ts
@@ -4,8 +4,27 @@ function getNumero(numero: number = 12):string{ //aqui asignamos al argumento un
 }
 console.log(getNumero(10));
 
+//Interface
+// Para hacer uso de una interface es necesario utilizar la palabra interface y asignar los valores que debe de tener la interface definidos
+interface CamisetaBase{
+    setColor(color);
+    getColor();
+}
+
+//Decoradores
+/* Es un patrón de diseño que nos permite mediante unos metadatos que nosotros le definimos de acuerdo a una clase hacer una copia de 
+esa misma clase y modificarla o que haga una cosa u otras en función de los parámetros que le pase de acuerdo.*/
+// function estampar(logo: string){
+//     return function(target: Function){
+//         target.prototype.estampacion = function():void{
+//             console.log("Camiseta estampada "+logo);
+//         }
+//     }
+// }
+
 //Clases (modelo de objeto)
-class Camiseta{
+// @estampar('CK')
+class Camiseta implements CamisetaBase{
     //propiedades (caracteristicas del objeto)
     private color: string;
     private modelo: string;
@@ -13,7 +32,17 @@ class Camiseta{
     private talla: string;
     public precio: number;
 
-    //Metodos (funciones o acciones del objeto)
+    /*Metodos (funciones o acciones del objeto)
+    el Constructor es un metodo especial dentro de una clase y se utiliza para darle un valor inicial a los atributos
+    o propiedades del objeto al crearlo, el constructor nunca devuelve ningun dato*/
+    constructor(color, modelo, marca, talla, precio){
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.talla = talla;
+        this.precio = precio;
+    }
+
     public setColor(color){
         this.color = color;
     }
@@ -21,14 +50,23 @@ class Camiseta{
         return this.color;
     }
 }
+// Clase hija(Ejemplo de herencia)
+// Es necesario agregar el extends para pder agregar las propiedades de la clase padre.
+class Sudadera extends Camiseta{
+    public capucha: boolean;
+    setCapucha(capucha: boolean){
+        this.capucha = capucha;
+    }
+    getCapucha():boolean{
+        return this.capucha;
+    }
+}
 
-var camiseta = new Camiseta();
+//Pasamos por parametro los atributos definidos dentro del constructor
+var camiseta = new Camiseta("Rojo", "Manga costa", "Polo","Grande", 14);
 camiseta.setColor("Rojo");
-camiseta.precio = 250;
-
-var playera = new Camiseta();
-playera.setColor("Azul");
-playera.precio = 500;
-
 console.log(camiseta);
-console.log(playera);
+
+var sudadera_nike = new Sudadera("Blanca", "Manga larga", "Sudadera", "Grande", 25);
+sudadera_nike.setCapucha(true);
+console.log(sudadera_nike);

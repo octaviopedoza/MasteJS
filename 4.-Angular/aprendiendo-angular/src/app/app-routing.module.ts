12f1a9a1ff90d55e969d54525235827ef13f4ea6
octaviopedroza-univer <octavio.pedroza@univer-gdl.edu.mx>
2022-11-17 10:32:09 -0600

    routing y actualizacion de angular

--- 4.-Angular/aprendiendo-angular/src/app/app-routing.module.ts
+++ 4.-Angular/aprendiendo-angular/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+// Modulos
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes
+import { CosinaComponent } from './cosina/cosina.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+// Array de rutas
+const routes: Routes = [
+  {path: '', component: CosinaComponent},
+  {path: 'cosina', component: CosinaComponent},
+  {path: 'videojuego', component: VideojuegoComponent},
+  {path: 'cursos', component: CursosComponent},
+  {path: '**', component: CosinaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
